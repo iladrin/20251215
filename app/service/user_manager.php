105ca3135607bed8b3e36f21c338a7031f0f802b
Benoit Tavernier <benoit.tavernier@gmail.com>
2025-12-16 16:49:56 +0100

    Utilisation des fonctions et variables static

--- app/service/user_manager.php
+++ app/service/user_manager.php
@@ -1,9 +1,26 @@
 <?php
 
+function getUsers(): array
+{
+    // Stockage des $users dans une mémoire "static", restaurée au prochain appel à cette fonction
+    static $users = null;
+
+    if ($users === null) {
+        // L’appel ci-dessous ne sera exécuté qu’au premier appel à cette fonction (variable "static")
+        $users = getUsersFromPhp();
+    }
+
+    return $users;
+}
+
+function getUsersFromPhp(): array
+{
+    return require DATA_PATH . '/users.php';
+}
 
 function userFind(int $id): ?array
 {
-    $users = require DATA_PATH . '/users.php';
+    $users = getUsers();
 
     foreach ($users as $user) {
         if ($user['id'] === $id) {
@@ -13,3 +30,4 @@ function userFind(int $id): ?array
 
     return null;
 }
+
